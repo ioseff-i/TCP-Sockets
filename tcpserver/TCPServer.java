51c4e3b984cc225ea0e396c50c4d8bf23323b439
Yusif Ibrahimov <52665836+ioseffufaz@users.noreply.github.com>
2019-07-29 16:05:58 +0400

    Update TCPServer.java

--- tcpserver/TCPServer.java
+++ tcpserver/TCPServer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tcp.server;
 
 import java.io.BufferedReader;
@@ -11,19 +7,14 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- *
- * @author user
- */
 public class TCPServer {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        ServerSocket serverSocket = new ServerSocket(1111);
-        while(true){
+        ServerSocket serverSocket = new ServerSocket(1111); //Xüsusən Server Socket üçün etdik Bunu
+        while(true) //Yuxarıda qeyd etmişdik ki, Server Daima açıq olmalıdır
+        {
             Socket mySocket = serverSocket.accept();
             InputStreamReader isr = new InputStreamReader(mySocket.getInputStream());
             BufferedReader  bw = new BufferedReader(isr);
