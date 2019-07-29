497a584d1e56e5172e03c5e6883c19d44cedfeb9
Yusif Ibrahimov <52665836+ioseffufaz@users.noreply.github.com>
2019-07-29 16:03:38 +0400

    Update TCPClient.java

--- tcpclient/TCPClient.java
+++ tcpclient/TCPClient.java
@@ -1,28 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tcp.client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
+import java.net.Socket; //Java-da Socketləri işə salmaq üçün  java.net package-i import edilməlidir
 import java.util.Scanner;
 
-/**
- *
- * @author user
- */
+
 public class TCPClient {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        Socket mySocket = new Socket("localhost",1111);
+        Socket mySocket = new Socket("localhost",1111);  // localhost - adresin öz kompüterim olduğunu göstərir
         DataOutputStream dat = new DataOutputStream(mySocket.getOutputStream());
         Scanner sc = new Scanner(System.in);
         String send = sc.nextLine();
